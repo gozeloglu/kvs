8e212a687eceb5c4a95177b986fca23d3b82d1f2
Gökhan Özeloğlu <gozeloglu@gmail.com>
2021-10-15 16:39:29 +0300

    Add tests for Set and Get

---
+++ get_test.go
@@ -0,0 +1,59 @@
+package kvs
+
+import (
+	"os"
+	"testing"
+)
+
+func TestKvs_GetEmpty(t *testing.T) {
+	db, err := Open(t.Name())
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	t.Logf("DB created.")
+
+	key := "foo"
+
+	v := db.Get(key)
+	if v != "" {
+		t.Fatalf("Value is wrong. Expected %s, got %s", "", v)
+	}
+	err = db.Close()
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	t.Logf("DB closed.")
+
+	err = os.RemoveAll(db.dir)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	t.Logf("Removed test directory.")
+}
+
+func TestKvs_Get(t *testing.T) {
+	db, err := Open(t.Name())
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	t.Logf("DB created.")
+
+	key, value := "foo", "bar"
+	db.Set(key, value)
+
+	v := db.Get(key)
+	if v != value {
+		t.Fatalf("Value is wrong. Expected %s, got %s", value, v)
+	}
+	err = db.Close()
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	t.Logf("DB closed.")
+
+	err = os.RemoveAll(db.dir)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	t.Logf("Removed test directory.")
+}
---
+++ set_test.go
@@ -0,0 +1,29 @@
+package kvs
+
+import (
+	"os"
+	"testing"
+)
+
+func TestKvs_Set(t *testing.T) {
+	db, err := Open(t.Name())
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	t.Logf("DB created.")
+
+	key, value := "foo", "bar"
+	db.Set(key, value)
+
+	err = db.Close()
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	t.Logf("DB closed.")
+
+	err = os.RemoveAll(db.dir)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	t.Logf("Test file removed.")
+}
